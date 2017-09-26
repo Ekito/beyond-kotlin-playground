f955d729e9b09dfc8e0eefff7e76b7929aa0e536
Arnaud Giuliani <agiuliani@ekito.fr>
2017-09-26 11:15:11 +0200

    tests prepare

---
+++ src/test/kotlin/fr/ekito/coroutines/AsyncWeatherTest.kt
@@ -0,0 +1,17 @@
+package fr.ekito.coroutines
+
+import org.junit.Test
+
+class AsyncWeatherTest {
+
+    @Test
+    fun testWeather() {
+
+    }
+
+    // Location / WS -> Deferred<Weather>
+    private fun asyncWeather() {}
+
+    // String location / WS -> Deferred<Geocode>
+    private fun asyncGeocode() {}
+}
---
+++ src/test/kotlin/fr/ekito/coroutines/ChannelWeatherTest.kt
@@ -0,0 +1,18 @@
+package fr.ekito.coroutines
+
+import org.junit.Test
+
+class ChannelWeatherTest {
+
+
+    @Test
+    fun testWeather(){
+
+    }
+
+    // Location / WS -> Deferred<Weather>
+    private fun channelWeather(){}
+
+    // String location / WS -> Deferred<Geocode>
+    private fun channelGeocode(){}
+}
